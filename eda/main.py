aaba896bc241e8b2faf61e9b59808ce57773c381
Kaniusii <kacper.woznica002@gmail.com>
2025-11-29 13:39:15 +0100

    pliki dzialaja

--- eda/main.py
+++ eda/main.py
@@ -1,10 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType, TimestampType
+from pyspark.sql.types import TimestampType
 
-# --------------------------------------------------
-# 1. Inicjalizacja SparkSession
-# --------------------------------------------------
 spark = (
     SparkSession.builder
     .appName("OccupancyDetectionEDA")
@@ -15,3 +12,37 @@ spark = (
 spark.sparkContext.setLogLevel("WARN")
 
 DATA_DIR = "dataset"
+
+
+def load_occupancy_file(filename: str, set_name: str):
+    path = f"{DATA_DIR}/{filename}"
+
+    df = (
+        spark.read
+        .option("header", "true")      # teraz nagłówek jest poprawny
+        .option("inferSchema", "true") # Spark sam rozpozna typy
+        .option("sep", ",")
+        .csv(path)
+        .withColumn("set_name", F.lit(set_name))
+    )
+
+    # Upewniamy się, że id jest intem (jakby Spark dał long/double)
+    df = df.withColumn("id", F.col("id").cast("int"))
+
+    # data -> Timestamp, bez kombinacji z unix_timestamp
+    df = df.withColumn("datetime", F.col("date").cast(TimestampType()))
+
+    return df
+
+train_df = load_occupancy_file("datatraining.txt", "train")
+test_df = load_occupancy_file("datatest.txt", "test")
+test2_df = load_occupancy_file("datatest2.txt", "test2")
+
+train_df.printSchema()
+train_df.show(5)
+
+test_df.printSchema()
+test_df.show(5)
+
+test2_df.printSchema()
+test2_df.show(5)
